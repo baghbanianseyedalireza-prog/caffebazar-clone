c78b3f441393058b743d070a928a54a4a40ca9c3
revelmood <baghbanianseyedalireza@gmail.com>
2025-12-14 20:35:22 +0330

    changed the Header little bit and fix some tailwind bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Footer from './components/Footer.jsx'
+import Header from './components/Header.jsx'
 import './index.css'
 
 function App() {
 
   return (
     <div className="App">
+      <Header/>
       {/* محتوای صفحه یا هدر */}
       <main className="font-display">
         <h1 className="text-2xl font-display text-center font-bold my-10">صفحه اصلی</h1>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,42 +1,42 @@
 import React from "react";
-import Logo from "./assets/images/bazaar-logo.svg";
+import Logo from "../assets/images/bazaar-logo.svg";
 const Header = () => {
   return (
-    <>
+
     <header className="flex items-center justify-around px-4 py-2 bg-white">
-      <img src={Logo} alt={Logo} className="h-10 cursor-pointer" />
+      <img src={Logo} alt="Bazaar Logo" className="h-10 cursor-pointer" />
       <div className="flex space-x-2">
         {/*دکمه برنامه */}
-        <button className="px-4 py-1 font-yekan text-[#464646] hover:text-[#2e2e2e] text-base/1.5 font-normal text-right cursor-pointer transition">
+        <button className="px-4 py-1 text-[#464646] hover:text-[#2e2e2e] text-base/1.5 font-normal text-right cursor-pointer transition">
           برنامه
         </button>
         {/*دکمه بازی */}
-        <button className="px-4 py-1 font-yekan text-[#464646] hover:text-[#2e2e2e] text-base/1.5 font-normal text-right cursor-pointer transitionr">
+        <button className="px-4 py-1 text-[#464646] hover:text-[#2e2e2e] text-base/1.5 font-normal text-right cursor-pointer transition">
           بازی
         </button>
         {/*دکمه فیلم و سریال */}
-        <button className="px-4 py-1 font-yekan text-[#464646] hover:text-[#2e2e2e] text-base/1.5 font-normal text-right cursor-pointer transition">
+        <button className="px-4 py-1 text-[#464646] hover:text-[#2e2e2e] text-base/1.5 font-normal text-right cursor-pointer transition">
           فیلم و سریال
         </button>
         {/*دکمه پشتیبانی */}
-        <button className="px-4 py-1 font-yekan text-[#464646] hover:text-[#2e2e2e] text-base/1.5 font-normal text-right cursor-pointer transition">
+        <button className="px-4 py-1 text-[#464646] hover:text-[#2e2e2e] text-base/1.5 font-normal text-right cursor-pointer transition">
           پشتیبانی
         </button>
         {/*دکمه توسعه دهندگان بازار */}
-        <button className="px-4 py-1 font-yekan text-[#464646] hover:text-[#2e2e2e] text-base/1.5 font-normal text-right cursor-pointer transition">
+        <button className="px-4 py-1 text-[#464646] hover:text-[#2e2e2e] text-base/1.5 font-normal text-right cursor-pointer transition">
           توسعه دهندگان بازار
         </button>
         {/*دکمه تبلیغات در بازار */}
-        <button className="px-4 py-1 font-yekan text-[#464646] hover:text-[#2e2e2e] text-base/1.5 font-normal text-right cursor-pointer transition">
+        <button className="px-4 py-1 text-[#464646] hover:text-[#2e2e2e] text-base/1.5 font-normal text-right cursor-pointer transition">
           تبلیغات در بازار
         </button>
         {/*دکمه مجله */}
-        <button className="px-4 py-1 font-yekan text-[#464646] hover:text-[#2e2e2e] text-base/1.5 font-normal text-right cursor-pointer transition">
+        <button className="px-4 py-1 text-[#464646] hover:text-[#2e2e2e] text-base/1.5 font-normal text-right cursor-pointer transition">
           مجله
         </button>
       </div>
       {/*دکمه دانلود بازار */}
-      <button className="px-[16px] text white text-right bg-[#0ea960] cursor-pointer border-[#0ea960] mr-[8px] text-gray-600 hover:text-gray-900">
+      <button className="px-[16px] text-right bg-[#0ea960] cursor-pointer border-[#0ea960] mr-[8px] text-gray-600 hover:text-gray-900">
         دانلود بازار
       </button>
       {/*نوار جست و جو*/}
@@ -51,7 +51,7 @@ const Header = () => {
       {/*آیکون کاربر */}
       <button className="p-2 text-gray-600 hover:text-gray-900">👤</button>
     </header>
-    </>
+
   );
 };
 
